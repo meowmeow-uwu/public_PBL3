7844ce16955db3c731158bd50ed7d1a37cc4bcce
Hoang Van Duong <hd2005555@gmail.com>
2025-05-11 15:12:05 +0700

    add to check role admin or content-manager by token

---
+++ src/main/java/com/pbl3/service/AuthService.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pbl3.service;
+
+import com.pbl3.dto.User;
+import com.pbl3.util.JwtUtil;
+
+/**
+ *
+ * @author Hoang Duong
+ */
+public class AuthService {
+
+    UserService userService = new UserService();
+
+    public boolean isContentManager(String authHeader) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return false;
+        }
+        String token = authHeader.substring("Bearer ".length()).trim();
+        int id = JwtUtil.getUserIdFromToken(token);
+        if (id == -1) {
+            return false;
+        }
+        User user = userService.selectByID(id);
+        return user != null && user.getGroup_user_id() == 3;
+    }
+
+    public boolean isAdmin(String authHeader) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return false;
+        }
+        String token = authHeader.substring("Bearer ".length()).trim();
+        int id = JwtUtil.getUserIdFromToken(token);
+        if (id == -1) {
+            return false;
+        }
+        User user = userService.selectByID(id);
+        return user != null && user.getGroup_user_id() == 1;
+    }
+}

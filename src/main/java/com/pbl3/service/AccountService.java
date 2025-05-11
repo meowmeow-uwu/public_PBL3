49f7343c2e4010779a3d4cce02a2dc24a96314fd
Hoang Van Duong <hd2005555@gmail.com>
2025-05-11 15:17:51 +0700

    add getAccountByToken(string token)

--- src/main/java/com/pbl3/service/AccountService.java
+++ src/main/java/com/pbl3/service/AccountService.java
@@ -30,7 +30,11 @@ public class AccountService implements ServiceInterface<Account> {
         return JwtUtil.generateToken(account.getUser_id());
     }
 
-    public Account selectByUserId(int id) {
+    public Account getAccountByToken(String token) {
+        int id = JwtUtil.getUserIdFromToken(token);
+        if (id == -1) {
+            throw new RuntimeException("Token không hợp lệ hoặc đã hết hạn.");
+        }
         return accountDAO.selectByUserId(id);
     }
 

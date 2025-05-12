bc1218474d0307ea3e49582ca84e72bc921aeb69
Hoang Van Duong <hd2005555@gmail.com>
2025-05-12 12:01:23 +0700

    implement service interface

---
+++ src/main/java/com/pbl3/service/DefinitionService.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pbl3.service;
+
+import com.pbl3.dao.DefinitionDAO;
+import com.pbl3.dto.Definition;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Hoang Duong
+ */
+public class DefinitionService implements ServiceInterface<Definition>{
+    private final DefinitionDAO definitionDAO = new DefinitionDAO();
+
+    @Override
+    public int insert(Definition t) {
+        int result = definitionDAO.insert(t);
+        return result;
+    }
+
+    @Override
+    public int update(Definition Definition) {
+        int result = definitionDAO.update(Definition);
+        return result;
+    }
+
+    @Override
+    public int delete(int wid) {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<Definition> selectAll() {
+        ArrayList<Definition> definitions = definitionDAO.selectAll();
+        return definitions;
+    }
+
+    @Override
+    public Definition selectByID(int id) {
+        Definition u = definitionDAO.selectByID(id);
+        if (u != null) {
+            return u;
+        }
+        return null;
+    }
+
+    @Override
+    public Definition selectByCondition(String condition) {
+        return null;
+    }
+
+}

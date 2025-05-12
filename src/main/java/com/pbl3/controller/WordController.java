466980efec6911ff94adf1a331b7093288377ac8
Hoang Van Duong <hd2005555@gmail.com>
2025-05-12 12:20:34 +0700

    change some function admin word to wordManagementController

--- src/main/java/com/pbl3/controller/WordController.java
+++ src/main/java/com/pbl3/controller/WordController.java
@@ -1,9 +1,15 @@
 package com.pbl3.controller;
 
+import com.pbl3.dto.User;
 import com.pbl3.dto.Word;
+import com.pbl3.service.AuthService;
+import com.pbl3.service.UserService;
 import com.pbl3.service.WordService;
+import com.pbl3.util.JwtUtil;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
@@ -19,27 +25,15 @@ import java.util.Map;
  */
 @Path("word")
 public class WordController {
-
+    private final UserService userService = new UserService();
+    private final AuthService authService = new AuthService();
     private final WordService wordService = new WordService();
-//@GET
-//@Path("{id}")
-//@Produces(MediaType.APPLICATION_JSON)
-//public Response getWord(@PathParam("id") int id) {
-//    Word Word = wordService.selectByID(id);
-//
-//    if (Word == null) {
-//        return Response.status(Response.Status.NOT_FOUND)
-//                       .entity("{\"error\":\"Word not found\"}")
-//                       .build();
-//    }
-//    return Response.ok(Word).build();
-//}
+
 
     @GET
     @Path("/{wordId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getWordDetail(@PathParam("wordId") int wordId) 
-    {
+    public Response getWordDetail(@PathParam("wordId") int wordId) {
         Map<String, Object> result = wordService.getWordDetail(wordId);
 
         if (result != null && !result.isEmpty()) {
@@ -51,38 +45,6 @@ public class WordController {
         }
     }
 
-    @POST
-    @Path("{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response insertWord(Word Word) {
-        int isInserted = wordService.insert(Word);
-
-        if (isInserted != 0) {
-            return Response.status(Response.Status.CREATED)
-                    .entity("{\"message\":\"Word created successfully\"}")
-                    .build();
-        } else {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("{\"error\":\"Failed to create Word\"}")
-                    .build();
-        }
-    }
-
-    @PUT
-    @Path("{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response updateWord(@PathParam("id") int id, Word Word) {
-        int isUpdated = wordService.update(Word);
-
-        if (isUpdated != 0) {
-            return Response.ok("{\"message\":\"Word updated successfully\"}").build();
-        } else {
-            return Response.status(Response.Status.NOT_FOUND)
-                    .entity("{\"error\":\"Word not found\"}")
-                    .build();
-        }
-    }
+    
 
 }

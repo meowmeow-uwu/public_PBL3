8ec14507d50beb4955ec691b7865924fbd180fe6
Hoang Van Duong <hd2005555@gmail.com>
2025-05-12 17:06:05 +0700

    add api create, delete, update definition

---
+++ src/main/java/com/pbl3/controller/DefinitionController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pbl3.controller;
+
+import jakarta.ws.rs.Path;
+
+/**
+ *
+ * @author Hoang Duong
+ */
+@Path("/definitions")
+public class DefinitionController {
+    
+}

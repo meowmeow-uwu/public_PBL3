5d342dbdf3150d882ba9f19758a98bcd54cfc7e4
Hoang Van Duong <hd2005555@gmail.com>
2025-05-14 16:19:17 +0700

    just user delete user from public collection , user cant insert/edit public collection , admin create collection allway public

--- src/main/java/com/pbl3/service/CollectionManagementService.java
+++ src/main/java/com/pbl3/service/CollectionManagementService.java
@@ -1,5 +1,8 @@
 package com.pbl3.service;
 
+import com.pbl3.dao.CollectionDAO;
+import com.pbl3.dao.CollectionOfUserDAO;
+import com.pbl3.dao.CollectionOfWordDAO;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,49 +10,54 @@ import java.util.Map;
 
 import com.pbl3.dto.Collection;
 
-public class CollectionManagementService extends CollectionService{
-        private static CollectionManagementService instance;
-        
+public class CollectionManagementService extends CollectionService {
 
-        
-        // Method để lấy instance
-        public static synchronized CollectionManagementService getInstance() {
-            if (instance == null) {
-                instance = new CollectionManagementService();
-            }
-            return instance;
+    private final CollectionDAO collectionDAO = CollectionDAO.getInstance();
+    private final CollectionOfWordDAO collectionOfWordDAO = CollectionOfWordDAO.getInstance();
+    private final CollectionOfUserDAO collectionOfUserDAO = CollectionOfUserDAO.getInstance();
+
+    private static CollectionManagementService instance;
+
+    // Method để lấy instance
+    public static synchronized CollectionManagementService getInstance() {
+        if (instance == null) {
+            instance = new CollectionManagementService();
         }
-    
-        
-    
-        // Lấy tất cả các bộ sưu tập công khai (dành cho content manager)
-        public List<Map<String, Object>> getAllPublicCollections() {
-            
-            List<Map<String, Object>> result = new ArrayList<>();
-            List<Collection> allCollections = selectAll();
-            
-            for (Collection collection : allCollections) {
-                if (collection.isPublic()) {
-                    Map<String, Object> collectionInfo = new HashMap<>();
-                    collectionInfo.put("collectionId", collection.getCollection_id());
-                    collectionInfo.put("name", collection.getCollection_name());
-                    collectionInfo.put("isPublic", collection.isPublic());
-                    collectionInfo.put("wordCount", wordCount(collection.getCollection_id()));
-                    result.add(collectionInfo);
-                }
-            }
-            return result;
-        }
-    
-        // Cập nhật trạng thái công khai của bộ sưu tập (dành cho content manager)
-        public boolean updateCollectionPublicStatus(int collectionId, boolean isPublic) {
-    
-            Collection collection = selectByID(collectionId);
-            if (collection != null) {
-                collection.setPublic(isPublic);
-                return update(collection) > 0;
-            }
-            return false;
-        }
-    
+        return instance;
     }
+
+    // Lấy tất cả các bộ sưu tập công khai (dành cho content manager)
+    public List<Map<String, Object>> getAllPublicCollections() {
+
+        List<Map<String, Object>> result = new ArrayList<>();
+        List<Collection> allCollections = selectAll();
+
+        for (Collection collection : allCollections) {
+            if (collection.isPublic()) {
+                Map<String, Object> collectionInfo = new HashMap<>();
+                collectionInfo.put("collectionId", collection.getCollection_id());
+                collectionInfo.put("name", collection.getCollection_name());
+                collectionInfo.put("isPublic", collection.isPublic());
+                collectionInfo.put("wordCount", wordCount(collection.getCollection_id()));
+                result.add(collectionInfo);
+            }
+        }
+        return result;
+    }
+
+    // Cập nhật trạng thái công khai của bộ sưu tập (dành cho content manager)
+    public boolean updateCollectionPublicStatus(int collectionId, boolean isPublic) {
+
+        Collection collection = selectByID(collectionId);
+        if (collection != null) {
+            collection.setPublic(isPublic);
+            return update(collection) > 0;
+        }
+        return false;
+    }
+
+    public int deleteWordFromCollection(String authHeader, int collection_id, int word_id) {
+        
+        return collectionOfWordDAO.deleteWordFromCollection(collection_id, word_id);
+    }
+}

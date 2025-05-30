9a27e88ea527e5cf5805420558410e44a8e9f1c9
OrcaPhan <patuananh11@gmail.com>
2025-05-30 11:05:40 +0700

    fix bug

--- src/main/webapp/Javascript/config.js
+++ src/main/webapp/Javascript/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
     BASE_PATH: './',  // Sẽ được ghi đè bởi mỗi trang
-    API_BASE_URL: 'http://localhost:6969/PBL3/api'
+    API_BASE_URL: 'http://localhost:2005/PBL3/api'
 };
 //const API = window.APP_CONFIG.API_BASE_URL
 // const API_LgRgt_URL = window.APP_CONFIG.API_BASE_URL + '/auth/';
